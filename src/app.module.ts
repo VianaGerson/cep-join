8f5237961e01dc5a3835bc454dad173153354c90
VianaGerson <gersonviana@rits.com.br>
2025-09-04 19:25:39 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import * as path from 'path';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), '..', 'assets'),
+      rootPath: path.join(process.cwd(), 'assets'),
       serveRoot: '/static',
     }),
   ],
